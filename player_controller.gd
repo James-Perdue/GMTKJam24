4b699e650323df6515e6cce14c0b5a642c3fd649
James Perdue <perdue.james@gmail.com>
2024-08-16 19:56:21 -0700

    Cells round 1

---
+++ player_controller.gd
@@ -0,0 +1,12 @@
+extends Node2D
+var colony: Node2D
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	colony = $Colony
+
+func _process(_delta: float) -> void:
+	var input_vector = Vector2.ZERO
+	input_vector.x = Input.get_action_strength("move_right") - Input.get_action_strength("move_left")
+	input_vector.y = Input.get_action_strength("move_down") - Input.get_action_strength("move_up")
+	colony.move(input_vector)
